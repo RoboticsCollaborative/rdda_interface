1be5ac311daf3a7d527f6b604ee06701f0da3b18
Stephenyu113 <stephenyu113@outlook.com>
2019-03-10 14:07:27 -0400

    python master node

--- scripts/RddProxy.py
+++ scripts/RddProxy.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 import rospy
-from rdda.msg import JointCommands
-from rdda.msg import JointStates
+from rdda_interface.msg import JointCommands
+from rdda_interface.msg import JointStates
 
 class RddProxy:
     def __init__(self):
 	self.joint_commands = None
 	self.joint_states = None
 
-	self.joint_pub = rospy.Publisher("rdd/joint_states", JointStates, queue_size = 1)
-	self.joint_sub = rospy.Subscriber("rdd/joint_commands", JointCommands, self.subJointStates_callback)
+	self.joint_pub = rospy.Publisher("rdd/joint_cmds", JointCommands, queue_size = 1)
+	self.joint_sub = rospy.Subscriber("rdd/joint_stats", JointStates, self.subJointStates_callback)
 
     def subJointStates_callBack(self, msg):
 	rospy.loginfo(rospy.get_caller_id() + "Joint States: %lf", msg.act_pos[0])
---
+++ scripts/master_node.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import rospy
+import math
+from rdda_interface.msg import JointCommands
+from rdda_interface.msg import JointStates
+#from RddProxy import RddProxy
+
+#master = RddProxy()
+
+#def subJointStates_callback(data):
+#    rospy.loginfo("Actual position: %lf", data.act_pos[0])
+
+i = 0
+
+"""
+def pubJointCommands():
+    msg = JointCommands()
+    if len(msg.tg_pos):
+	msg.tg_pos[0] = 3.14159
+    else:
+	msg.tg_pos.append(3.14159)
+    joint_pub.publish(msg)
+    rospy.loginfo("Actual position: %lf", msg.tg_pos[0])
+"""
+
+def run():
+    global i
+    msg = JointCommands()
+    while not rospy.is_shutdown():
+	pos = math.cos(2*3.14159/100 * i)
+        if len(msg.tg_pos):
+   	    msg.tg_pos[0] = pos
+    	else:
+	    msg.tg_pos.append(pos)
+	i += 1
+    	joint_pub.publish(msg)
+    	rospy.loginfo("Actual position: %lf", msg.tg_pos[0])
+#	rospy.spin()
+	rate.sleep()
+
+if __name__ == '__main__':
+    try:
+    	joint_pub = rospy.Publisher("rdd/joint_cmds", JointCommands, queue_size = 1)
+    	rospy.init_node('ros_master', anonymous=True)
+    	rate = rospy.Rate(1000)
+#	rospy.Subscriber("rdd/joint_stats", JointStates, subJointStates_callback)
+	run()
+    except rospy.ROSInterruptException:
+	pass
