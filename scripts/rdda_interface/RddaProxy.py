57cce3dd6a80fbaa92f3f1e0a9e5961127cc3196
StephenYu <stephenyu113@outlook.com>
2019-07-04 21:14:48 -0400

    added homing function to RddaProxy

--- scripts/rdda_interface/RddaProxy.py
+++ scripts/rdda_interface/RddaProxy.py
@@ -2,6 +2,7 @@ import rospy
 from rdda_interface.msg import JointCommands
 from rdda_interface.msg import JointStates
 
+import time
 import numpy as np
 
 
@@ -9,7 +10,7 @@ class RddaProxy:
 
     def __init__(self):
         self.joint_pub = rospy.Publisher("rdd/joint_cmds", JointCommands, queue_size=1)
-        # self.joint_sub = rospy.Subscriber("rdd/joint_stats", JointStates, self.subjointstates_callback)
+        self.joint_sub = rospy.Subscriber("rdd/joint_stats", JointStates, self.subjointstates_callback)
 
         """Joint states"""
         self.act_pos = None
@@ -18,28 +19,57 @@ class RddaProxy:
         self.ts_nsec = None
         self.ts_sec = None
 
-        """Joint commands"""
-        self.pos_ref = np.array([0.0, 0.0])
-        self.vel_sat = np.array([3.0, 3.0])
-        self.tau_sat = np.array([5.0, 5.0])
-        self.stiffness = np.array([0.1, 0.1])
-        self.freq_anti_alias = 500.0
+    def subjointstates_callback(self, msg):
+        self.act_pos = msg.act_pos
+        self.act_vel = msg.act_vel
+        self.act_tau = msg.act_tau
+        self.ts_nsec = msg.ts_nsec
+        self.ts_sec = msg.ts_sec
 
-    # def subjointstates_callback(self, msg):
-    #     self.act_pos = msg.data.act_pos
-    #     self.act_vel = msg.data.act_vel
-    #     self.act_tau = msg.data.act_tau
-    #     self.ts_nsec = msg.data.ts_nsec
-    #     self.ts_sec = msg.data.ts_sec
+    def publish_joint_cmds(self, pos_ref=(0.0, 0.0), vel_sat=(3.0, 3.0),
+                           tau_sat=(5.0, 5.0), stiffness=(0.0, 0.0), freq_anti_alias=500.0):
 
-    def publish_joint_cmds(self):
         joint_cmd_msg = JointCommands()
-        joint_cmd_msg.pos_ref = self.pos_ref
-        joint_cmd_msg.vel_sat = self.vel_sat
-        joint_cmd_msg.tau_sat = self.tau_sat
-        joint_cmd_msg.stiffness = self.stiffness
-        joint_cmd_msg.freq_anti_alias = self.freq_anti_alias
+        joint_cmd_msg.pos_ref = pos_ref
+        joint_cmd_msg.vel_sat = vel_sat
+        joint_cmd_msg.tau_sat = tau_sat
+        joint_cmd_msg.stiffness = stiffness
+        joint_cmd_msg.freq_anti_alias = freq_anti_alias
 
         self.joint_pub.publish(joint_cmd_msg)
 
+    def homing(self):
+        pos_ref = np.array([0.0, 0.0])
+        stiffness = np.array([5, 5])
+        rate = rospy.Rate(500)
+        time_interval = 0.0
+        tau_threshold = np.array([0.14, 0.14])
+        done_finger0 = False
+        done_finger1 = False
+        done = False
 
+        while not done and not rospy.is_shutdown():
+            time_interval += 2e-6
+            # pos_ref[0] = -1.0 * np.sin(time_interval)
+            tau_measured = self.act_tau
+
+            if not done_finger0:
+                if tau_measured[0] > tau_threshold[0]:
+                    pos_ref[0] += 1.27
+                    done_finger0 = True
+                else:
+                    pos_ref[0] += -1.0 * time_interval
+            if not done_finger1:
+                if tau_measured[1] > tau_threshold[1]:
+                    pos_ref[1] += 1.15
+                    done_finger1 = True
+                else:
+                    pos_ref[1] += -1.0 * time_interval
+
+            self.publish_joint_cmds(pos_ref=pos_ref, stiffness=stiffness)
+
+            # rospy.loginfo("pos_ref[0]: {}".format(pos_ref[0]))
+            rate.sleep()
+            done = done_finger0 and done_finger1
+
+        time.sleep(1)
--- scripts/rdda_interface/homing.py
+++ scripts/rdda_interface/homing.py
@@ -8,16 +8,19 @@ from RddaProxy import RddaProxy
 def main():
     rdda = RddaProxy()
     rospy.init_node('homing', anonymous=True)
-    rate = rospy.Rate(500)
-    time_interval = 0.0
+    # rate = rospy.Rate(500)
+    # time_interval = 0.0
 
-    while not rospy.is_shutdown():
-        time_interval += 2e-3
-        rdda.pos_ref[0] = -1.0 * np.sin(time_interval)
-        rdda.publish_joint_cmds()
-        rospy.loginfo("pos_ref[0]: {}".format(rdda.pos_ref[0]))
-        rate.sleep()
+    # pos_ref = np.array([0.0, 0.0])
+    # stiffness = np.array([5, 5])
 
+    # while not rospy.is_shutdown():
+    #     time_interval += 2e-3
+    #     pos_ref[0] = -1.0 * np.sin(time_interval)
+    #     rdda.publish_joint_cmds(pos_ref=pos_ref, stiffness=stiffness)
+    #     rospy.loginfo("pos_ref[0]: {}".format(pos_ref[0]))
+    #     rate.sleep()
+    rdda.homing()
 
 if __name__ == '__main__':
     try:
