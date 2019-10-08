728cce0a8b1a9509cbab04fe45afc1a9d7a026c4
Stephenyu113 <stephenyu113@outlook.com>
2019-10-08 15:23:21 -0400

    updated rosparam after homing

--- scripts/rdda_interface/homing.py
+++ scripts/rdda_interface/homing.py
@@ -10,6 +10,9 @@ def main():
 
     rdda.homing()
 
+    rospy.set_param('/rdda_interface/origins', rdda.joint_origins)
+    rospy.set_param('/rdda_interface/upper_bounds', rdda.joint_upper_bounds)
+    rospy.set_param('/rdda_interface/lower_bounds', rdda.joint_lower_bounds)
 
 if __name__ == '__main__':
     try:
