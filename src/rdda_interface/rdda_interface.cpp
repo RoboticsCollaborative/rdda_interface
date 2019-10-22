fff02b5e83e7d307eb6e1df354a5a97c4ebe7df3
steph <stephenyu113@outlook.com>
2019-10-22 01:07:57 -0400

    added option for homing in launch file

--- src/rdda_interface/rdda_interface.cpp
+++ src/rdda_interface/rdda_interface.cpp
@@ -15,8 +15,6 @@ RDDNode::RDDNode(ros::NodeHandle &node, Rdda *rddaptr) {
     rdda_maxvel_srv = nh_.advertiseService("set_max_vel", &RDDNode::setMaxVel, this);
     rdda_maxeff_srv = nh_.advertiseService("set_max_eff", &RDDNode::setMaxEffort, this);
     rdda_stiff_srv = nh_.advertiseService("set_stiff", &RDDNode::setStiffness, this);
-
-//    rdda_homing_cli = nh_.serviceClient<rdda_interface::Homing>("/rdda_interface/homing");
 }
 
 RDDNode::~RDDNode() = default;
@@ -44,20 +42,6 @@ void RDDNode::initConfigParams() {
     }
 
     mutex_unlock(&rdda->mutex);
-
-//    if (ros::param::get("~need_homing", need_homing)) {
-//        std::vector<double> origins;
-//        rdda_interface::Homing homing_srv;
-//        homing_srv.request.need_homing = need_homing;
-//        if (rdda_homing_cli.call(homing_srv)) {
-//            for (int i=0; i<2; ++i) {
-//                origins[i] = homing_srv.response.origins[i];
-//                ros::param::set("~origins", origins);
-//            }
-//            ROS_INFO("Joint Origins: [%lf, %lf]", origins[0], origins[1]);
-//        }
-//    }
-
 }
 
 /* Publish joint state */
